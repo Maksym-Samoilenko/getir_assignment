71aed45b441dd121b2c330c6286e6f7a668203e1
Maksym-Samoilenko <maksym.samoilenko@gmail.com>
2020-07-13 00:44:08 +0300

    Fixed db_string to mongooseDb

--- dao/mongoose.js
+++ dao/mongoose.js
@@ -3,9 +3,9 @@
  * @author maxsam125@google.com (Maksym Samoilenko)
  */
 const mongoose = require('mongoose');
-const db_string = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
+const mongooseDb = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
 try {
-   mongoose.connect(db_string, { useNewUrlParser: true, useUnifiedTopology: true });
+   mongoose.connect(mongooseDb, { useNewUrlParser: true, useUnifiedTopology: true });
    require('../model/record');
 } catch (error) {
    console.error(error);
