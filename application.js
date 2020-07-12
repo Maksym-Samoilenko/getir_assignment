08e24430cb4a4e52d82f9e6a9af9d9f366d3e793
Maksym-Samoilenko <maksym.samoilenko@gmail.com>
2020-07-13 00:36:32 +0300

    added comment

--- application.js
+++ application.js
@@ -1,4 +1,9 @@
 'use strict'
+/**
+ * @fileoverview Functionality for deploying server 
+ * with express application 
+ * @author maxsam125@google.com (Maksym Samoilenko)
+ */
 const app = require("./server");
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
