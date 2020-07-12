6b4f05ab7f6aa029ba4cd70c4dc01eb639213529
Maksym-Samoilenko <maksym.samoilenko@gmail.com>
2020-07-13 00:40:44 +0300

    added comment

--- validation/utils.js
+++ validation/utils.js
@@ -1,19 +1,31 @@
 /**
- * @fileoverview Validation utils
+ * @fileoverview
+ * @module utils, which contains validation utils functions
  * @author maxsam125@google.com (Maksym Samoilenko)
  */
 const moment = require('moment');
 module.exports = {
+    /**
+    * @param first {Date}
+    * @param second {Date}
+    */
     isFirstBeforeSecondDate: (first, second) => {
       // isFirstBeforeSecondDate returns true if first date is after second date
         return moment(first, "YYYY-MM-DD", true).isValid() &&
             moment(second, "YYYY-MM-DD", true).isValid() &&
             moment(second).isSameOrAfter(first);
     },
+    /**
+    * @param number {number}
+    */
     isIntegerGtEZero: (number) => {
         // isIntegerGtEZero returns true if number >= 0
         return (Number.isInteger(number) && number >= 0);
     },
+    /**
+    * @param first {number}
+    * @param second {number}
+    */
     isFirstLtESecondNumber: (first, second) => {
         // isFirstLtESecondNumber returns true if first <= second
         return first <= second;
